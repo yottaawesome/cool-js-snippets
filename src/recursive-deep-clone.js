755604b328535f2e9d4e310831c01f66e01af365
Vasilios Magriplis <etameson@gmail.com>
2019-08-22 12:24:33 +1000

    Create recursive-deep-clone.js

---
+++ src/recursive-deep-clone.js
@@ -0,0 +1,28 @@
+// Adapted from https://jsperf.com/deep-copy-vs-json-stringify-json-parse/5
+function recursiveDeepCopy(o) {
+  var newO,
+    i;
+
+  if (typeof o !== 'object') {
+    return o;
+  }
+  if (!o) {
+    return o;
+  }
+
+  if ('[object Array]' === Object.prototype.toString.apply(o)) {
+    newO = [];
+    for (i = 0; i < o.length; i += 1) {
+      newO[i] = recursiveDeepCopy(o[i]);
+    }
+    return newO;
+  }
+
+  newO = {};
+  for (i in o) {
+    if (o.hasOwnProperty(i)) {
+      newO[i] = recursiveDeepCopy(o[i]);
+    }
+  }
+  return newO;
+}
