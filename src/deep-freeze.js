66d5b95fd77a7389b4350bd61fada8fb47e0ef25
Vasilios Magriplis <etameson@gmail.com>
2019-09-05 07:21:59 +1000

    Create deep-freeze.js

---
+++ src/deep-freeze.js
@@ -0,0 +1,15 @@
+// Adapted from https://github.com/substack/deep-freeze/blob/master/index.js
+function deepFreeze (o) {
+  Object.freeze(o);
+
+  Object.getOwnPropertyNames(o).forEach(function (prop) {
+    if (o.hasOwnProperty(prop)
+    && o[prop] !== null
+    && (typeof o[prop] === "object" || typeof o[prop] === "function")
+    && !Object.isFrozen(o[prop])) {
+      deepFreeze(o[prop]);
+    }
+  });
+  
+  return o;
+};
